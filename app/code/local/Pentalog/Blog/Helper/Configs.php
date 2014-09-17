2b6ee1589df4da5957d36d0cbc45f957ab9f2656
Nguyen Do Trong <ndotrong@pentalog.fr>
2014-09-17 10:56:09 +0300

    Blog Module: Create configuration in admin

--- app/code/local/Pentalog/Blog/Helper/Configs.php
+++ app/code/local/Pentalog/Blog/Helper/Configs.php
@@ -17,32 +17,39 @@ class Pentalog_Blog_Helper_Configs extends Mage_Core_Helper_Abstract {
         if ($this->_configs === null) {
             $this->_configs = array(
                 //Blog Listing config
-                'blog_list_image_width' => '150px',
-                'blog_list_image_height' => '150px',
-                'column_article' => 4,
-                'toolbar_default_direction' => 'desc',
-                'toolbar_default_sort_by' => 'blog_id',
+                'blog_list_image_width' => ($this->getConfig('blog_section/blog_config/blog_list_image_width'))? $this->getConfig('blog_section/blog_config/blog_list_image_width') : '150px',
+                'blog_list_image_height' => ($this->getConfig('blog_section/blog_config/blog_list_image_height'))? $this->getConfig('blog_section/blog_config/blog_list_image_height') : '150px',
+                'column_article' => ($this->getConfig('blog_section/blog_config/column_article'))? $this->getConfig('blog_section/blog_config/column_article') : 4,
+                'toolbar_default_direction' => ($this->getConfig('blog_section/blog_config/toolbar_default_direction'))? $this->getConfig('blog_section/blog_config/toolbar_default_direction') : 'DESC',
+                'toolbar_default_sort_by' => ($this->getConfig('blog_section/blog_config/toolbar_default_sort_by'))? $this->getConfig('blog_section/blog_config/toolbar_default_sort_by') : 'blog_id',
                 'toolbar_order_list' => array(
                     'blog_id' => 'Newest',
                     'title' => 'Title',
                 ),
-                'toolbar_show_per_page' => array(4 => 4, 8 => 8, 12 => 12),
+//                'toolbar_show_per_page' => array(4 => 4, 8 => 8, 12 => 12),
                 //Blog Detail config
-                'blog_detail_image_width' => '350px',
-                'blog_detail_image_height' => '350px',
-                'default_article_image' => 'xpentalog/blog/defaultImage.jpg',
+                'blog_detail_image_width' => ($this->getConfig('blog_section/blog_config/blog_detail_image_width'))? $this->getConfig('blog_section/blog_config/blog_detail_image_width') : '350px',
+                'blog_detail_image_height' => ($this->getConfig('blog_section/blog_config/blog_detail_image_height'))? $this->getConfig('blog_section/blog_config/blog_detail_image_height') : '350px',
+                'default_article_image' => ($this->getConfig('blog_section/blog_config/default_article_image'))? $this->getConfig('blog_section/blog_config/default_article_image') : 'xpentalog/blog/defaultImage.jpg',
                 //Blog comment config
-                'allow_not_login_write_comment' => true,
+                'allow_not_login_write_comment' => ($this->getConfig('blog_section/blog_config/allow_not_login_write_comment'))? $this->getConfig('blog_section/blog_config/allow_not_login_write_comment') : 1,
                 //Blog Category List
-                'blog_category_list_image_width' => '150px',
-                'blog_category_list_image_height' => '150px',
-                'blog_category_view_image_width' => '100%',
-                'blog_category_view_image_height' => '200px',
+                'blog_category_list_image_width' => ($this->getConfig('blog_section/blog_category/blog_category_list_image_width'))? $this->getConfig('blog_section/blog_category/blog_category_list_image_width') : '150px',
+                'blog_category_list_image_height' => ($this->getConfig('blog_section/blog_category/blog_category_list_image_height'))? $this->getConfig('blog_section/blog_category/blog_category_list_image_height') : '150px',
+                'blog_category_view_image_width' => ($this->getConfig('blog_section/blog_category/blog_category_view_image_width'))? $this->getConfig('blog_section/blog_category/blog_category_view_image_width') : '150px',
+                'blog_category_view_image_height' => ($this->getConfig('blog_section/blog_category/blog_category_view_image_height'))? $this->getConfig('blog_section/blog_category/blog_category_view_image_height') : '150px',
                 //Blog general
-                'blog_title' => 'Pentalog Blog',
-                'blog_active' => $this->getHelper()->getConfigValue('blog_section/general/active', $store),
-                'blog_layout' => $this->getHelper()->getConfigValue('blog_section/general/layout', $store),
+                'blog_title' => ($this->getConfig('blog_section/general/blog_title'))? $this->getConfig('blog_section/general/blog_title') : 'Pentalog Blog',
+                'blog_active' => ($this->getConfig('blog_section/general/blog_active'))? $this->getConfig('blog_section/general/blog_active') : 1,
+                'blog_layout' => ($this->getConfig('blog_section/general/blog_layout'))? $this->getConfig('blog_section/general/blog_layout') : 'page/1column.phtml',
             );
+
+            if($this->getConfig('blog_section/blog_config/toolbar_show_per_page')){
+                $this->_configs['toolbar_show_per_page'] = explode(",", $this->getConfig('blog_section/blog_config/toolbar_show_per_page'));
+            }else{
+                $this->_configs['toolbar_show_per_page'] = array(4 => 4, 8 => 8, 12 => 12);
+            }
+
             //Convert configs to Object
             $config = new Varien_Object();
             $this->_configs = $config->addData($this->_configs);
@@ -58,7 +65,7 @@ class Pentalog_Blog_Helper_Configs extends Mage_Core_Helper_Abstract {
      */
     public function getConfig($path){
         $storeId = Mage::app()->getStore()->getStoreId();
-        return $this->getHelper()->getConfigValue($path, $store);
+        return $this->getHelper()->getConfigValue($path, $storeId);
     }
 
 }
